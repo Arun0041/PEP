e87e01bdf0e2e8ecea3735e8ab2844048fd7e33a
Arun0041 <arungautam0041@gmail.com>
2026-02-06 16:58:50 +0530

    Day 9

---
+++ Stack/introduction.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+class stack{
+    public:
+    vector<int> ans;
+    int t=-1;
+    void push(int val){
+        t++;
+        ans.push_back(val);
+    }
+    int top(){
+        return ans[t];
+    }
+    void pop(){
+        t--;
+        ans.pop_back();
+    }
+    int size(){
+        return ans.size();
+    }
+};
+int main(){
+    stack st;
+    st.push(5);
+    st.push(10);
+    st.push(55);
+    st.push(2);
+    st.push(1);
+    cout<<st.top()<<endl;
+    st.pop();
+    cout<<st.top();
+}
