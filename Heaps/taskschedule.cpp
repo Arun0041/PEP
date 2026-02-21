aacb7840f92fc8ef22c022c7689169dee004eae0
Arun0041 <arungautam0041@gmail.com>
2026-02-21 23:42:14 +0530

    Task Schedule

---
+++ Heaps/taskschedule.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int> freq(26,0);
+        for(char c : tasks) freq[c-'A']++;
+
+        priority_queue<int> pq;
+        for(int f : freq)
+            if(f) pq.push(f);
+
+        int time = 0;
+
+        while(!pq.empty()){
+            vector<int> temp;
+            int cycle = n + 1;
+
+            while(cycle-- && !pq.empty()){
+                int f = pq.top();
+                pq.pop();
+                if(f-1 > 0) temp.push_back(f-1);
+                time++;
+            }
+
+            for(int x : temp) pq.push(x);
+
+            if(pq.empty()) break;
+
+            time += cycle + 1;
+        }
+
+        return time;
+    }
+};
